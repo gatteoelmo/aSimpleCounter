db872edcd0e5d984c7e6d6a782695aaa5d204633
gatteoelmo <matteogallardo99@gmail.com>
2024-04-19 19:08:10 +0100

    ok lo mando di nuovo

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-let add;
-let subract;
-let output;
+let counter = document.querySelector("div");
+let add = document.createElement("button");
+let output = document.createElement("span");
+let subract = document.createElement("button");
+
+subract.textContent = "-";
+output.textContent = "0";
+add.textContent = "+";
+counter.appendChild(subract);
+counter.appendChild(output);
+counter.appendChild(add);
 
 add.addEventListener("click", function () {
   let result = Number(output.innerText) + 1;
